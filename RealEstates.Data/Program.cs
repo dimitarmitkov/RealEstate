9c8dcf9ea492a970b18a70cf62c6eb90e15019d0
dimitarmitkov <55533699+dimitarmitkov@users.noreply.github.com>
2020-07-25 17:06:06 +0300

    Added Files In Structure

---
+++ RealEstates.Data/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace RealEstates.Data
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RealEstates.Data/RealEstateDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RealEstates.Data
+{
+    public class RealEstateDbContext : DbContext
+    {
+
+
+    }
+}
