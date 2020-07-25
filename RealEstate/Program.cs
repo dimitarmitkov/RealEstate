b593f35e9b09f817c2da1dc5bad5c66fadd391b8
dimitarmitkov <55533699+dimitarmitkov@users.noreply.github.com>
2020-07-25 15:59:44 +0300

    Create Project Structure

---
+++ RealEstate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RealEstate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
